f26fc2131b3d9b25a2ae2f738bf1f0b415d8f6f1
Krutika Vijay Patil <kpatil3@uncc.edu>
2017-06-14 00:07:21 -0400

    Create MoveZeroesSoln.java

---
+++ Move Zeroes/MoveZeroesSoln.java
@@ -0,0 +1,26 @@
+public class Solution 
+{
+    public void moveZeroes(int[] nums) 
+    {
+        int count=0;
+        
+        for(int i=0;i<nums.length;i++)
+        {
+           
+            if(nums[i]==0)
+            {
+                count++;
+            }
+            else 
+            {
+                nums[i-count]=nums[i];
+            }
+            
+        }
+        for(int i=1;i<=count;i++)
+        {
+            nums[nums.length-i]=0;
+        }
+        
+    }
+}
